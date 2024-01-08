c9184a09b57a23673861d926a786ecae358d7373
humbertodlacerda <humbertodlacerda@gmail.com>
2024-01-08 20:51:21 -0300

    feat: Add opening routes logic

---
+++ router/routes.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "GET Opening",
+			})
+		})
+
+		v1.POST("/opening", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "POST Opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "DELETE Opening",
+			})
+		})
+
+		v1.PUT("/opening", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "PUT Opening",
+			})
+		})
+
+		v1.GET("/openings", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "GET Opening",
+			})
+		})
+	}
+}
