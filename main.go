6072431c3eb6992b8f5113195c513c9e872592f3
humbertodlacerda <humbertodlacerda@gmail.com>
2024-01-08 21:55:49 -0300

    feat: Add opening schema package

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/humbertodlacerda/gopportunities/router"
+import (
+	"github.com/humbertodlacerda/gopportunities/config"
+	"github.com/humbertodlacerda/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize router
 	router.Initialize()
 }
