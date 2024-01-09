1b3c92f6e4d5d0458e33226fc059779a6fbc6547
humbertodlacerda <humbertodlacerda@gmail.com>
2024-01-08 22:29:59 -0300

    feat: add database response handler

--- schemas/opening.go
+++ schemas/opening.go
@@ -2,6 +2,7 @@ package schemas
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Opening struct {
@@ -12,4 +13,17 @@ type Opening struct {
 	Remote   bool
 	Link     string
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty"`
 }
