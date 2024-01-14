0d7a9ef32e2005ebbd2a3d4a1a4571431a3775fb
humbertodlacerda <humbertodlacerda@gmail.com>
2024-01-13 22:17:15 -0300

    feat: add createOpening handler logic

--- handler/CreateOpening.go
+++ handler/CreateOpening.go
@@ -2,11 +2,36 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/humbertodlacerda/gopportunities/schemas"
 	"net/http"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	context.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Link:     request.Link,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(context, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(context, "create-opeing", opening)
+	return
 }
--- router/router.go
+++ router/router.go
@@ -1,14 +1,35 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func Initialize() {
 	// Initialize router
 	router := gin.Default()
+
+	router.Use(corsMiddleware())
 
 	// Initialize routes
 	initializeRoutes(router)
 
 	// Run the server
 	router.Run(":8080")
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		// Continuar para o próximo middleware ou rota
+		c.Next()
+	}
 }
