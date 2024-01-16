a53509efa8097f79c856c779dc25140dd0fe8558
humbertodlacerda <humbertodlacerda@gmail.com>
2024-01-16 12:34:11 -0300

    apply logic crud

--- handler/ListOpening.go
+++ handler/ListOpening.go
@@ -2,11 +2,17 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/humbertodlacerda/gopportunities/schemas"
 	"net/http"
 )
 
 func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(context, "list-opening", openings)
 }
--- handler/ShowOpening.go
+++ handler/ShowOpening.go
@@ -1,12 +1,26 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/humbertodlacerda/gopportunities/schemas"
 	"net/http"
 )
 
 func ShowOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	id := context.Query("id")
+
+	if id == "" {
+		sendError(context, http.StatusBadRequest, errorParamIsRequired(id, "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(context, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(context, "show-opening", opening)
 }
